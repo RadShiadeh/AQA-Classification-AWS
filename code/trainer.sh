19b3c356aacb1dc4becaca903e686add145bf691
Radmehr Shiadeh <78790490+Glonki4@users.noreply.github.com>
2024-03-12 14:11:04 +0000

    different dataloader so I dont use cv2

--- code/trainer.sh
+++ code/trainer.sh
@@ -15,10 +15,5 @@
 module purge
 # load in the module dependencies for this script
 module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
-module load python/3.5.2-gcc4
-module load cuda/8.0
-module load cudnn/5.1
-module load hdf5/1.10.0-patch1
 
-python3 -c 'import cv2'
 python trainer.py
