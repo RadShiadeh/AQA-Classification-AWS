4bfb76f252c64f31e8820793d5c0323bfba572d3
Radmehr Shiadeh <78790490+Glonki4@users.noreply.github.com>
2024-03-13 16:26:03 +0000

    bash script to include cv2

--- code/trainer.sh
+++ code/trainer.sh
@@ -11,17 +11,7 @@
 #SBATCH --mem=4GB
 #SBATCH --cpus-per-task=10
 
-# get rid of any modules already loaded
-module purge
-# load in the module dependencies for this script
 module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
-conda info --envs
-
-# activate the conda environment
-source activate my_conda
 
 # run your Python script
 python trainer.py
-
-# deactivate the conda environment
-conda deactivate
