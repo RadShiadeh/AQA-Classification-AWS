9826b156b662843248ba992a642096df3286db4f
Radmehr Shiadeh <78790490+Glonki4@users.noreply.github.com>
2024-03-16 17:46:48 +0000

    logs

--- code/dataLoader.py
+++ code/dataLoader.py
@@ -22,27 +22,19 @@ class VideoDataset(Dataset):
         video_id = self.video_ids[idx]
         video_path = os.path.join(self.root_dir, f"{video_id}.mp4")
 
-        # Read video frames
         frames = self.read_video_frames(video_path)
 
-        # Resize frames to a consistent size
         frames_resized = [transforms.functional.resize(Image.fromarray(frame), self.resize_shape) for frame in frames]
 
-        # Convert frames_resized to a torch tensor
         frames_tensor = torch.stack([transforms.functional.to_tensor(frame) for frame in frames_resized])
 
-        # Ensure the number of frames is consistent
         if len(frames_tensor) < self.num_frames:
-            # Padding frames with zeros
             frames_tensor = torch.cat([frames_tensor, torch.zeros(self.num_frames - len(frames_tensor), 3, *self.resize_shape)], dim=0)
         elif len(frames_tensor) > self.num_frames:
-            # Trimming frames
             frames_tensor = frames_tensor[:self.num_frames]
 
-        # Get classification and score from labels
         classification_label, score_label = self.labels[video_id]
 
-        # Convert labels to torch tensors
         classification_label_tensor = torch.tensor(classification_label, dtype=torch.float32).view(1)
         score_label_tensor = torch.tensor(score_label, dtype=torch.float32).view(1)
 
@@ -54,14 +46,12 @@ class VideoDataset(Dataset):
         return labels
 
     def read_video_frames(self, video_path):
-        # Use OpenCV to read video frames
         cap = cv2.VideoCapture(video_path)
         frames = []
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
-            # Convert BGR to RGB
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frames.append(frame)
         cap.release()
--- code/trainer.py
+++ code/trainer.py
@@ -1,3 +1,4 @@
+import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -6,11 +7,43 @@ from torch.utils.tensorboard import SummaryWriter
 from model import C3DC, FullyConnected, ScoreRegressor, EndToEndModel, ClassifierCNN3D
 from dataLoader import VideoDataset
 
+step = 0
+log_frequency = 2
+
+def print_metrics(epoch, loss, data_load_time, step_time, loss_type):
+        epoch_step = step % len(video_dataset)
+        print(
+                f"epoch: [{epoch}], "
+                f"step: [{epoch_step}/{len(video_dataset)}], "
+                f"batch loss: {loss:.5f}, "
+                #f"AUC: {auc_score}, "
+                f"data load time: "
+                f"{data_load_time:.5f}, "
+                f"step time: {step_time:.5f}",
+                f"for {loss_type}"
+        )
+
+
+def log_metrics(epoch, loss, data_load_time, step_time):
+    summary_writer.add_scalar("epoch", epoch, step)
+    summary_writer.add_scalars(
+                "loss",
+            {"train": float(loss.item())},
+            step
+    )
+    summary_writer.add_scalar(
+            "time/data", data_load_time, step
+    )
+    summary_writer.add_scalar(
+            "time/data", step_time, step
+    )
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.device("cpu")
+
+print_frequency = 2
 
 classifier = ClassifierCNN3D()
 
@@ -23,7 +56,7 @@ fc = FullyConnected()
 score_reg = ScoreRegressor()
 fc = fc.to(device)
 score_reg = score_reg.to(device)
-batch_size = 20
+batch_size = 10
 
 data_loader = DataLoader(video_dataset, batch_size=batch_size, shuffle=True)
 
@@ -31,18 +64,28 @@ eteModel = EndToEndModel(classifier, cnnLayer, fc, final_score_regressor=score_r
 eteModel = eteModel.to(device)
 
 criterion_classification = nn.BCELoss()
+
 criterion_scorer = nn.CrossEntropyLoss()
-optimizer = optim.AdamW(eteModel.parameters(), lr=0.00001)
+
+all_params = (list(fc.parameters()) + list(cnnLayer.parameters()) + list(score_reg.parameters()) + list(classifier.parameters()))
+optimizer = optim.AdamW(all_params, lr=0.0001)
 
 summary_writer = SummaryWriter()
 
-num_epochs = 50
+num_epochs = 5
 for epoch in range(num_epochs):
+    print('-------------------------------------------------------------------------------------------------------')
     for i, batch_data in enumerate(data_loader):
+        data_load_start_time = time.time()
+
+        eteModel.train()
+
         frames = batch_data[0].type(torch.FloatTensor).to(device)
         frames = frames.permute(0, 2, 1, 3, 4)
         classification_labels = batch_data[1].type(torch.FloatTensor).to(device)
         score_labels = batch_data[2].type(torch.FloatTensor).to(device)
+
+        data_load_end_time = time.time()
         
         optimizer.zero_grad()
             
@@ -55,12 +98,20 @@ for epoch in range(num_epochs):
 
         classification_loss.backward()
         final_score_loss.backward()
+
         optimizer.step()
 
-        global_step = epoch * len(data_loader) + i
-        summary_writer.add_scalar('classification loss', classification_loss.item(), global_step)
-        summary_writer.add_scalar('score loss', final_score_loss.item(), global_step)
+        data_load_time = data_load_end_time - data_load_start_time
+        step_time = time.time() - data_load_end_time
+        if ((step + 1) % log_frequency) == 0:
+            log_metrics(epoch, classification_loss, data_load_time, step_time)
+            log_metrics(epoch, final_score_loss, data_load_time, step_time)
+        if ((step + 1) % print_frequency) == 0:
+            print_metrics(epoch,  classification_loss, data_load_time, step_time, "classification")
+            print_metrics(epoch,  final_score_loss, data_load_time, step_time, "scorer")
 
-    print(f'Epoch {epoch + 1}/{num_epochs}, classification loss: {classification_loss.item()}, final score loss: {final_score_loss.item()}')
+        step += 1
+
+    #print(f'Epoch {epoch + 1}/{num_epochs}, classification loss: {classification_loss.item()}, final score loss: {final_score_loss.item()}')
 
 summary_writer.close()
