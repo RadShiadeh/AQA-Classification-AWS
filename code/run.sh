ad8c045c9a1d9f4952788dd0506e5a84279205a5
Radmehr Shiadeh <78790490+Glonki4@users.noreply.github.com>
2024-03-27 11:20:21 +0000

    requesting more resources on bc4

---
+++ code/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=ETE_AQA
+#SBATCH --partition=gpu
+#SBATCH --nodes 1
+#SBATCH --time=48:00:0
+#SBATCH --gres=gpu:2  # Requesting 2 GPUs
+#SBATCH -o ./log_%j.out # STDOUT out
+#SBATCH -e ./log_%j.err # STDERR out
+#SBATCH --account=COMS030144
+#SBATCH --mem=16gb
+
+echo start time is "$(date)"
+echo Slurm job ID is "${SLURM_JOBID}"
+
+module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
+
+# run your Python script
+python -u trainer.py
+
+echo end time is "$(date)"
+hostname
